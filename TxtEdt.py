7f27620a4bb0f3626fe791528a94df2cc2cc8cc9
obszank <obszank@gmail.com>
2024-10-13 18:20:13 +0200

    Update TxtEdt.py

--- TxtEdt.py
+++ TxtEdt.py
@@ -1,5 +1,7 @@
 import os
 
+# print("Dir: " + os.getcwd())
+
 def main():
     print("Welcome to TxtEdt!")
     print("Type your text. Write SAVE <filename> to save and QUIT to exit")
@@ -11,10 +13,17 @@ def main():
 
         if line.startswith("SAVE "):
             filename = line.split(" ", 1)[1]
-            savepath = f"../Documents/{filename}"
-            with open(savepath, "w") as f:
-                f.write("\n".join(text))
-            print(f"File {filename} saved successfully")
+            if __name__ == '__main__':
+                savepath = f"../Documents/{filename}"
+            else:
+                savepath = f"Documents/{filename}"
+            try:
+
+                with open(savepath, "w") as f:
+                    f.write("\n".join(text))
+                print(f"File {filename} saved successfully")
+            except Exception as e:
+                print(f"ERR saving the file: {e}")
 
         elif line.strip().upper() == "QUIT":
             break
